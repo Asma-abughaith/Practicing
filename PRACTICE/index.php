5860408e0e752c261eb4c5b2fafd76d22c3807c6
asma <asma.abughaith@gmail.com>
2022-11-19 08:00:36 +0300

    new

---
+++ PRACTICE/index.php
@@ -0,0 +1,82 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel = 'stylesheet' href="./assets/style.css">
+</head>
+  <body>
+    <div id="main-container" class='d-flex mt-5 '>
+        <div id="main-1" class='d-flex'>
+            <a href='' id='a-1'>
+                <img class="number number1" src='./assets/images/1.png' width='256px' height='256px'  >
+                <img src='./assets/images/1_1.jpg' width='787.38px' height='439.84px'>
+            </a>
+        </div>
+        <div id="main-2" class='d-flex '>
+            <a href='' id='a-2' class='d-flex '>
+                <img class="number number2" src='./assets/images/2.png' width='256px' height='256px' >
+                <img src='./assets/images/2.jpg' width='787.38px' height='439.84px'>
+            </a>
+        </div>
+        <div id="main-3" class='d-flex '>
+            <a href='' id='a-3' class='d-flex '>
+                <img class="number number3" src='./assets/images/3.png' width='256px' height='256px' >
+                <img src='./assets/images/3.jpg' width='787.38px' height='439.84px'>
+            </a>
+        </div>
+        <div id="main-4" class='d-flex  '>
+            <a href='' id='a-4' class='d-flex '>
+                <img class="number number4" src='./assets/images/4.png' width='256px' height='256px' >
+                <img src='./assets/images/4.jpg' width='787.38px' height='439.84px'>
+            </a>
+        </div>
+        <div id="main-5" class='d-flex  '>
+            <a href='' id='a-5' class='d-flex '>
+                <img class="number number5" src='./assets/images/5.png' width='256px' height='256px' >
+                <img src='./assets/images/5.jpg' width='787.38px' height='439.84px'>
+            </a>
+        </div>
+        <div id="main-6" class='d-flex '>
+            <a href='' id='a-6' class='d-flex '>
+                <img class="number number6 " src='./assets/images/6.png' width='256px' height='256px' >
+                <img src='./assets/images/6.jpg' width='787.38px' height='439.84px'>
+            </a>
+        </div>
+        <div id="main-7" class='d-flex  '>
+            <a href='' id='a-7' class='d-flex '>
+                <img class="number number7" src='./assets/images/7.png' width='256px' height='256px' >
+                <img src='./assets/images/7.jpg' width='787.38px' height='439.84px'>
+            </a>
+        </div>
+        <div id="main-8" class='d-flex '>
+            <a href='' id='a-8' class='d-flex '>
+                <img class="number number8" src='./assets/images/8.png' width='256px' height='256px' >
+                <img src='./assets/images/8.jpg' width='787.38px' height='439.84px'>
+            </a>
+        </div>
+        <div id="main-9" class='d-flex  '>
+            <a href='' id='a-9'class='d-flex '>
+                <img class="number number9" src='./assets/images/9.png' width='256px' height='256px' >
+                <img src='./assets/images/9.jpg' width='787.38px' height='439.84px'>
+            </a>
+        </div>
+        <div id="main-10" class='d-flex '>
+        <a href='' id='a-10'class='d-flex '>
+            <img class="number number10" src='./assets/images/10.png' width='256px' height='256px' >
+            <img src='./assets/images/10.jpg' width='787.38px' height='439.84px'>
+        </a>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    <script src="netflix.js"></script>
+        
+   
+  </body>
+</html>
---
+++ PRACTICE/netflix.js
@@ -0,0 +1,197 @@
+
+// $(function () {
+//  let counter=1;
+
+
+//  $(`#a-${counter}`). click(function (e) {
+//     e.preventDefault();
+//     $(`#a-${counter++}`).css("width", "50px");
+//     $(`#a-${counter}`).css("width", "50px");
+//  });
+
+// });
+
+
+// $(function () {
+// let counter=1;
+
+// $(`#a-${counter}`).click(function (e) {
+//     e.preventDefault();
+//     $(this).parent().css("width", "50px");
+//     counter++;
+// });
+// });
+
+
+
+
+
+$(`#a-1`). click(function (e) {
+       e.preventDefault();
+      
+        $(`#main-1`).css("width", "1100px");
+        //$(`.number1`).css("display", "block");
+        $( "main-2" ).hover(function() {
+            $( this ).fadeOut( 50 );
+            $( this ).fadeIn(150);
+          });
+    
+        $(`#main-3`).css("width", "50px");
+        $(`#main-4`).css("width", "50px");
+        $(`#main-5`).css("width", "50px");
+        $(`#main-6`).css("width", "50px");
+        $(`#main-7`).css("width", "50px");
+        $(`#main-8`).css("width", "50px");
+        $(`#main-9`).css("width", "50px");
+        $(`#main-10`).css("width", "50px");
+        
+
+  });
+
+  $(`#a-2`). click(function (e) {
+    e.preventDefault();
+   
+    $(`#main-2`).css("width", "1100px");
+    //$(`.number2`).css("display", "block");
+
+    
+    $(`#main-1`).css("width", "50px");
+    $(`#main-3`).css("width", "50px");
+    $(`#main-4`).css("width", "50px");
+    $(`#main-5`).css("width", "50px");
+    $(`#main-6`).css("width", "50px");
+    $(`#main-7`).css("width", "50px");
+    $(`#main-8`).css("width", "50px");
+    $(`#main-9`).css("width", "50px");
+    $(`#main-10`).css("width", "50px");
+});
+
+$(`#a-3`). click(function (e) {
+    e.preventDefault();
+   
+    $(`#main-3`).css("width", "1100px");
+        //$(`.number1`).css("display", "block");
+        $(`#main-2`).css("width", "50px");
+        $(`#main-1`).css("width", "50px");
+        $(`#main-4`).css("width", "50px");
+        $(`#main-5`).css("width", "50px");
+        $(`#main-6`).css("width", "50px");
+        $(`#main-7`).css("width", "50px");
+        $(`#main-8`).css("width", "50px");
+        $(`#main-9`).css("width", "50px");
+        $(`#main-10`).css("width", "50px");
+});
+
+$(`#a-4`). click(function (e) {
+    e.preventDefault();
+   
+    $(`#main-4`).css("width", "1100px");
+    //$(`.number1`).css("display", "block");
+    $(`#main-2`).css("width", "50px");
+    $(`#main-3`).css("width", "50px");
+    $(`#main-1`).css("width", "50px");
+    $(`#main-5`).css("width", "50px");
+    $(`#main-6`).css("width", "50px");
+    $(`#main-7`).css("width", "50px");
+    $(`#main-8`).css("width", "50px");
+    $(`#main-9`).css("width", "50px");
+    $(`#main-10`).css("width", "50px");
+});
+
+$(`#a-5`). click(function (e) {
+    e.preventDefault();
+   
+    $(`#main-5`).css("width", "1100px");
+    //$(`.number1`).css("display", "block");
+    $(`#main-2`).css("width", "50px");
+    $(`#main-3`).css("width", "50px");
+    $(`#main-4`).css("width", "50px");
+    $(`#main-1`).css("width", "50px");
+    $(`#main-6`).css("width", "50px");
+    $(`#main-7`).css("width", "50px");
+    $(`#main-8`).css("width", "50px");
+    $(`#main-9`).css("width", "50px");
+    $(`#main-10`).css("width", "50px");
+});
+
+$(`#a-6`). click(function (e) {
+    e.preventDefault();
+   
+    $(`#main-6`).css("width", "1100px");
+    //$(`.number1`).css("display", "block");
+    $(`#main-2`).css("width", "50px");
+    $(`#main-3`).css("width", "50px");
+    $(`#main-4`).css("width", "50px");
+    $(`#main-5`).css("width", "50px");
+    $(`#main-1`).css("width", "50px");
+    $(`#main-7`).css("width", "50px");
+    $(`#main-8`).css("width", "50px");
+    $(`#main-9`).css("width", "50px");
+    $(`#main-10`).css("width", "50px");
+});
+$(`#a-7`). click(function (e) {
+    e.preventDefault();
+   
+    $(`#main-7`).css("width", "1100px");
+    //$(`.number1`).css("display", "block");
+    $(`#main-2`).css("width", "50px");
+    $(`#main-3`).css("width", "50px");
+    $(`#main-4`).css("width", "50px");
+    $(`#main-5`).css("width", "50px");
+    $(`#main-6`).css("width", "50px");
+    $(`#main-1`).css("width", "50px");
+    $(`#main-8`).css("width", "50px");
+    $(`#main-9`).css("width", "50px");
+    $(`#main-10`).css("width", "50px");
+});
+
+$(`#a-8`). click(function (e) {
+    e.preventDefault();
+   
+    $(`#main-8`).css("width", "1100px");
+    //$(`.number1`).css("display", "block");
+    $(`#main-2`).css("width", "50px");
+    $(`#main-3`).css("width", "50px");
+    $(`#main-4`).css("width", "50px");
+    $(`#main-5`).css("width", "50px");
+    $(`#main-6`).css("width", "50px");
+    $(`#main-7`).css("width", "50px");
+    $(`#main-1`).css("width", "50px");
+    $(`#main-9`).css("width", "50px");
+    $(`#main-10`).css("width", "50px");
+});
+
+$(`#a-9`). click(function (e) {
+    e.preventDefault();
+   
+    $(`#main-9`).css("width", "1100px");
+    //$(`.number1`).css("display", "block");
+    $(`#main-2`).css("width", "50px");
+    $(`#main-3`).css("width", "50px");
+    $(`#main-4`).css("width", "50px");
+    $(`#main-5`).css("width", "50px");
+    $(`#main-6`).css("width", "50px");
+    $(`#main-7`).css("width", "50px");
+    $(`#main-8`).css("width", "50px");
+    $(`#main-1`).css("width", "50px");
+    $(`#main-10`).css("width", "50px");
+});
+
+$(`#a-10`). click(function (e) {
+    e.preventDefault();
+   
+    $(`#main-10`).css("width", "1100px");
+    //$(`.number1`).css("display", "block");
+    $(`#main-2`).css("width", "50px");
+    $(`#main-3`).css("width", "50px");
+    $(`#main-4`).css("width", "50px");
+    $(`#main-5`).css("width", "50px");
+    $(`#main-6`).css("width", "50px");
+    $(`#main-7`).css("width", "50px");
+    $(`#main-8`).css("width", "50px");
+    $(`#main-9`).css("width", "50px");
+    $(`#main-1`).css("width", "50px");
+});
+
+
+
